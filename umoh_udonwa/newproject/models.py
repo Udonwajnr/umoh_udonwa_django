a7ae3e9c03ffa997105a7343d8f2512bd2521d29
udonwajnr <udonwajnr10@gmail.com>
2022-06-01 20:39:32 +0100

    first django project

---
+++ umoh_udonwa/newproject/models.py
@@ -0,0 +1,10 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+class Schools(models.Model):
+    name = models.CharField(max_length = 23)
+    address = models.CharField(max_length=23)
+    
+class Country(models.Model):
+    name = models.CharField(max_length=23)
